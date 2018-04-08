8ba9528f8235044223cd88de80423ba8489655b9
pazetox <pazetox@gmail.com>
2018-04-08 01:28:58 -0300

    criação rain tlp e index ,footer e header , page.php

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 	
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("select * from tb_users");
-	echo json_encode($results);
+	//$sql = new Hcode\DB\Sql();
+	//$results = $sql->select("select * from tb_users");
+	//echo json_encode($results);
 	//echo "OK";
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
